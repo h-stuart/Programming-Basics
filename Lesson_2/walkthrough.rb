d9f9085c31e88a0853adde625d4be6c5d91f4a55
Harold Stuart <harold.stuart@tripleseat.com>
2023-04-17 12:49:39 -0400

    Walkthrough Caluclator

---
+++ Lesson_2/walkthrough.rb
@@ -0,0 +1,34 @@
+# This program: 
+# asks for two numbers
+# asks for the type of operation to perform: add, subtract, multiply or divide
+# performs the operation
+# displays the result
+
+#answer = Kernel.gets()
+#Kernel.puts(answer)
+
+Kernel.puts("Welcome to the Calculator!")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+    result = number1.to_i() + number2.to_i()
+
+elsif operator == '2'
+    result = number1.to_i() - number2.to_i()
+
+elsif operator == '3'
+    result = number1.to_i() * number2.to_i()
+
+elsif operator == '4'
+    result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}!")
